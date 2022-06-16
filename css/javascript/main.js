6bb8208834d74800c407a3e05977d735d89b5ed6
Vanbaelinghem Alan <Vanbaelinghem.Alan@gmail.com>
2022-06-16 14:52:15 +0200

    progress in the fire (active button list)

--- css/javascript/main.js
+++ css/javascript/main.js
@@ -1,10 +1,21 @@
 // const navBurger = document.querySelectorAll("fa fa-bars")
 // const navList = document.querySelectorAll("nav-list")
 
-//     navBurger.addEventListener("click", function(event) {
-//         this.classList.toggle("active");
-//     })
-function toggleIcon(event) {
-    this.classList.toggle("fa fa-angle-down");
-}
-document.querySelector("fa.fa-bars").addEventListener("click", toggleIcon);
+// //     navBurger.addEventListener("click", function(event) {
+// //         this.classList.toggle("active");
+// //     })
+
+// function toggleIcon(event) {
+//     this.classList.toggle("active");
+// }
+
+// document.querySelector("fa.fa-bars").addEventListener("click", toggleIcon);
+
+
+
+const navBurger = document.querySelector(".nav .nav-burger")
+const navList = document.querySelector('.nav-list')
+
+navBurger.addEventListener("click", function() {
+    navList.classList.toggle("active");
+})
