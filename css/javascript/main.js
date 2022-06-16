047df92dba756b02460a335a3a1d0100068ad5b7
Vanbaelinghem Alan <Vanbaelinghem.Alan@gmail.com>
2022-06-16 15:59:02 +0200

    JavaHell (ep 2 : togglebutton)

--- css/javascript/main.js
+++ css/javascript/main.js
@@ -15,7 +15,15 @@
 
 const navBurger = document.querySelector(".nav .nav-burger")
 const navList = document.querySelector('.nav-list')
+const iconChange = document.querySelector(".fa")
 
 navBurger.addEventListener("click", function() {
     navList.classList.toggle("active");
+    if (navList == "nav-list active")
+    iconChange.classList.replace("fa-bars","fa-angle-up")
+    else {
+    iconChange.classList.replace("fa-angle-up","fa-bars")
+//     console.log(iconChange)}
+    }
 })
+
