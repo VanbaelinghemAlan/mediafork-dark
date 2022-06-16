94ac817804bd277e987673d7e9d4aa2b11c6b2eb
Vanbaelinghem Alan <Vanbaelinghem.Alan@gmail.com>
2022-06-16 16:09:08 +0200

    Javahell (ep 3 : The switch button works)

--- css/javascript/main.js
+++ css/javascript/main.js
@@ -1,29 +1,13 @@
-// const navBurger = document.querySelectorAll("fa fa-bars")
-// const navList = document.querySelectorAll("nav-list")
-
-// //     navBurger.addEventListener("click", function(event) {
-// //         this.classList.toggle("active");
-// //     })
-
-// function toggleIcon(event) {
-//     this.classList.toggle("active");
-// }
-
-// document.querySelector("fa.fa-bars").addEventListener("click", toggleIcon);
-
-
-
 const navBurger = document.querySelector(".nav .nav-burger")
 const navList = document.querySelector('.nav-list')
 const iconChange = document.querySelector(".fa")
 
 navBurger.addEventListener("click", function() {
     navList.classList.toggle("active");
-    if (navList == "nav-list active")
+    if (navList.classList.contains("active"))
     iconChange.classList.replace("fa-bars","fa-angle-up")
     else {
-    iconChange.classList.replace("fa-angle-up","fa-bars")
-//     console.log(iconChange)}
+    iconChange.classList.replace("fa-angle-up","fa-bars")  
     }
 })
 
