2c98c1bc151d16778c1658b14c404e66655c861b
Vanbaelinghem Alan <Vanbaelinghem.Alan@gmail.com>
2022-06-16 14:02:23 +0200

    a little better with margin (still freestyle YOLO)

---
+++ css/javascript/main.js
@@ -0,0 +1,9 @@
+const navBurger = document.querySelectorAll("fa fa-bars")
+const navList = document.querySelectorAll("nav-list")
+
+    navBurger.addEventListener("click", function(event) {
+        this.classList.toggle("active");
+
+
+        navList.forEach()
+    })
